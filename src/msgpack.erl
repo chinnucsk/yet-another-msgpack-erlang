5cce605d4f38c8c3fad7e621821a88747deec58f
Ryosuke Nakai <twisted.mind@voluntas.net>
2012-07-16 15:19:57 +0900

    Rename pack_/unpack_ to pack_term/unpack_term

--- src/msgpack.erl
+++ src/msgpack.erl
@@ -2,6 +2,8 @@
 
 -export([pack/1, unpack/1]).
 
+-export([pack_term/1, unpack_term/1]).
+
 -include_lib("eunit/include/eunit.hrl").
 
 -type msgpack_map() :: [{msgpack_term(), msgpack_term()}].
@@ -15,82 +17,82 @@ unpack(Term) ->
     {error, {badarg, Term}}.
 
 unpack(<<>>, []) ->
-    {error, {incomplete, <<>>}};
+    {error, incomplete};
 unpack(<<>>, [Term]) ->
     {ok, Term};
 unpack(<<>>, Acc) ->
     {ok, lists:reverse(Acc)};
 unpack(Binary, Acc) ->
-    case unpack_(Binary) of
+    case unpack_term(Binary) of
         {ok, Term, Rest} ->
             unpack(Rest, [Term|Acc]);
         {error, Reason} ->
             {error, Reason}
     end.
 
--spec unpack_(binary()) -> {ok, msgpack_term(), binary()} | {error, term()}.
+-spec unpack_term(binary()) -> {ok, msgpack_term(), binary()} | {error, term()}.
 %% atom
-unpack_(<<16#C0, Rest/binary>>) ->
+unpack_term(<<16#C0, Rest/binary>>) ->
     {ok, nil, Rest};
-unpack_(<<16#C2, Rest/binary>>) ->
+unpack_term(<<16#C2, Rest/binary>>) ->
     {ok, false, Rest};
-unpack_(<<16#C3, Rest/binary>>) ->
+unpack_term(<<16#C3, Rest/binary>>) ->
     {ok, true, Rest};
 %% float
-unpack_(<<16#CA, V:32/float-unit:1, Rest/binary>>) ->
+unpack_term(<<16#CA, V:32/float-unit:1, Rest/binary>>) ->
     {ok, V, Rest};
-unpack_(<<16#CB, V:64/float-unit:1, Rest/binary>>) ->
+unpack_term(<<16#CB, V:64/float-unit:1, Rest/binary>>) ->
     {ok, V, Rest};
 %% unsigned integer
-unpack_(<<16#CC, V:8/unsigned-integer, Rest/binary>>) ->
+unpack_term(<<16#CC, V:8/unsigned-integer, Rest/binary>>) ->
     {ok, V, Rest};
-unpack_(<<16#CD, V:16/big-unsigned-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#CD, V:16/big-unsigned-integer-unit:1, Rest/binary>>) ->
     {ok, V, Rest};
-unpack_(<<16#CE, V:32/big-unsigned-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#CE, V:32/big-unsigned-integer-unit:1, Rest/binary>>) ->
     {ok, V, Rest};
-unpack_(<<16#CF, V:64/big-unsigned-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#CF, V:64/big-unsigned-integer-unit:1, Rest/binary>>) ->
     {ok, V, Rest};
 %% signed integer
-unpack_(<<16#D0, V:8/signed-integer, Rest/binary>>) ->
+unpack_term(<<16#D0, V:8/signed-integer, Rest/binary>>) ->
     {ok, V, Rest};
-unpack_(<<16#D1, V:16/big-signed-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#D1, V:16/big-signed-integer-unit:1, Rest/binary>>) ->
     {ok, V, Rest};
-unpack_(<<16#D2, V:32/big-signed-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#D2, V:32/big-signed-integer-unit:1, Rest/binary>>) ->
     {ok, V, Rest};
-unpack_(<<16#D3, V:64/big-signed-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#D3, V:64/big-signed-integer-unit:1, Rest/binary>>) ->
     {ok, V, Rest};
 %% raw byte
-unpack_(<<16#DA, L:16/unsigned-integer-unit:1, V:L/binary, Rest/binary>>) ->
+unpack_term(<<16#DA, L:16/unsigned-integer-unit:1, V:L/binary, Rest/binary>>) ->
     {ok, V, Rest};
-unpack_(<<16#DB, L:32/unsigned-integer-unit:1, V:L/binary, Rest/binary>>) ->
+unpack_term(<<16#DB, L:32/unsigned-integer-unit:1, V:L/binary, Rest/binary>>) ->
     {ok, V, Rest};
 %% array
-unpack_(<<16#DC, L:16/big-unsigned-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#DC, L:16/big-unsigned-integer-unit:1, Rest/binary>>) ->
     unpack_array(Rest, L, []);
-unpack_(<<16#DD, L:32/big-unsigned-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#DD, L:32/big-unsigned-integer-unit:1, Rest/binary>>) ->
     unpack_array(Rest, L, []);
 %% map
-unpack_(<<16#DE, L:16/big-unsigned-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#DE, L:16/big-unsigned-integer-unit:1, Rest/binary>>) ->
     unpack_map(Rest, L, []);
-unpack_(<<16#DF, L:32/big-unsigned-integer-unit:1, Rest/binary>>) ->
+unpack_term(<<16#DF, L:32/big-unsigned-integer-unit:1, Rest/binary>>) ->
     unpack_map(Rest, L, []);
 %% Tag-encoded lengths (kept last, for speed)
-unpack_(<<0:1, V:7, Rest/binary>>) ->
+unpack_term(<<0:1, V:7, Rest/binary>>) ->
     {ok, V, Rest};
 %% negative int
-unpack_(<<2#111:3, V:5, Rest/binary>>) ->
+unpack_term(<<2#111:3, V:5, Rest/binary>>) ->
     {ok, V - 2#100000, Rest};
 % raw bytes
-unpack_(<<2#101:3, L:5, V:L/binary, Rest/binary>>) ->
+unpack_term(<<2#101:3, L:5, V:L/binary, Rest/binary>>) ->
     {ok, V, Rest};
 %% array
-unpack_(<<2#1001:4, L:4, Rest/binary>>) ->
+unpack_term(<<2#1001:4, L:4, Rest/binary>>) ->
     unpack_array(Rest, L, []);
 %% map
-unpack_(<<2#1000:4, L:4, Rest/binary>>) ->
+unpack_term(<<2#1000:4, L:4, Rest/binary>>) ->
     unpack_map(Rest, L, []);
 %% Invalid data
-unpack_(Binary) ->
+unpack_term(Binary) ->
     case binary:first(Binary) of
         16#C1 ->
             {error, {badarg, Binary}};
@@ -119,14 +121,14 @@ unpack_(Binary) ->
         16#D9 ->
             {error, {badarg, Binary}};
         _ ->
-            {error, {incomplete, Binary}}
+            {error, incomplete}
     end.
 
 -spec unpack_array(binary(), non_neg_integer(), [msgpack_term()]) -> {ok, [msgpack_term()], binary()} | {error, term()}.
 unpack_array(Rest, 0, Acc) ->
     {ok, lists:reverse(Acc), Rest};
 unpack_array(Binary, Len, Acc) ->
-    case unpack_(Binary) of
+    case unpack_term(Binary) of
         {ok, Term, Rest} ->
             unpack_array(Rest, Len - 1, [Term|Acc]);
         Error ->
@@ -137,9 +139,9 @@ unpack_array(Binary, Len, Acc) ->
 unpack_map(Rest, 0, Acc) ->
     {ok, lists:reverse(Acc), Rest};
 unpack_map(Binary, Len, Acc) ->
-    case unpack_(Binary) of
+    case unpack_term(Binary) of
         {ok, Key, Rest1} ->
-            case unpack_(Rest1) of
+            case unpack_term(Rest1) of
                 {ok, Value, Rest2} ->
                     unpack_map(Rest2, Len - 1, [{Key, Value}|Acc]);
                 Error ->
@@ -151,9 +153,9 @@ unpack_map(Binary, Len, Acc) ->
 
 -spec pack(msgpack_term() | [msgpack_term()]) -> {ok, binary()} | {error, term()}.
 pack(<<>>) ->
-    {error, {incomplete, <<>>}};
+    {error, incomplete};
 pack(Terms) ->
-    case pack_(Terms) of
+    case pack_term(Terms) of
         {ok, Binary} ->
             {ok, Binary};
         {error, _Reason} when is_list(Terms) ->
@@ -165,68 +167,68 @@ pack(Terms) ->
 pack([], Acc) ->
     {ok, list_to_binary(lists:reverse(Acc))};
 pack([Term|Rest], Acc) ->
-    case pack_(Term) of
+    case pack_term(Term) of
         {ok, Binary} ->
             pack(Rest, [Binary|Acc]);
         {error, Reason} ->
             {error, Reason}
     end.
 
--spec pack_(msgpack_term()) -> binary() | no_return().
-pack_(I) when is_integer(I) andalso I < 0 ->
-    pack_int_(I);
-pack_(I) when is_integer(I) ->
-    pack_uint_(I);
-pack_(F) when is_float(F) ->
+-spec pack_term(msgpack_term()) -> binary() | no_return().
+pack_term(I) when is_integer(I) andalso I < 0 ->
+    pack_int(I);
+pack_term(I) when is_integer(I) ->
+    pack_uint(I);
+pack_term(F) when is_float(F) ->
     pack_double(F);
-pack_(nil) ->
+pack_term(nil) ->
     {ok, <<16#C0:8>>};
-pack_(true) ->
+pack_term(true) ->
     {ok, <<16#C3:8>>};
-pack_(false) ->
+pack_term(false) ->
     {ok, <<16#C2:8>>};
-pack_(Bin) when is_binary(Bin) ->
+pack_term(Bin) when is_binary(Bin) ->
     pack_raw(Bin);
-pack_([{_, _}|_] = Map) ->
+pack_term([{_, _}|_] = Map) ->
     pack_map(Map);
-pack_(List) when is_list(List) ->
+pack_term(List) when is_list(List) ->
     pack_array(List);
-pack_(Term) ->
+pack_term(Term) ->
     {error, {badarg, Term}}.
 
 
--spec pack_uint_(non_neg_integer()) -> binary().
+-spec pack_uint(non_neg_integer()) -> binary().
 %% positive fixnum
-pack_uint_(N) when N =< 16#7F ->
+pack_uint(N) when N =< 16#7F ->
     {ok, <<2#0:1, N:7>>};
 %% uint 8
-pack_uint_(N) when N =< 16#FF ->
+pack_uint(N) when N =< 16#FF ->
     {ok, <<16#CC:8, N:8>>};
 %% uint 16
-pack_uint_(N) when N =< 16#FFFF ->
+pack_uint(N) when N =< 16#FFFF ->
     {ok, <<16#CD:8, N:16/big-unsigned-integer-unit:1>>};
 %% uint 32
-pack_uint_(N) when N < 16#FFFFFFFF ->
+pack_uint(N) when N < 16#FFFFFFFF ->
     {ok, <<16#CE:8, N:32/big-unsigned-integer-unit:1>>};
 %% uint 64
-pack_uint_(N) ->
+pack_uint(N) ->
     {ok, <<16#CF:8, N:64/big-unsigned-integer-unit:1>>}.
 
--spec pack_int_(integer()) -> binary().
+-spec pack_int(integer()) -> binary().
 %% negative fixnum
-pack_int_(N) when N >= -32->
+pack_int(N) when N >= -32 ->
     {ok, <<2#111:3, N:5>>};
 %% int 8
-pack_int_(N) when N > -128 ->
+pack_int(N) when N > -128 ->
     {ok, <<16#D0:8, N:8/big-signed-integer-unit:1>>};
 %% int 16
-pack_int_(N) when N > -32768 ->
+pack_int(N) when N > -32768 ->
     {ok, <<16#D1:8, N:16/big-signed-integer-unit:1>>};
 %% int 32
-pack_int_(N) when N > -16#FFFFFFFF ->
+pack_int(N) when N > -16#FFFFFFFF ->
     {ok, <<16#D2:8, N:32/big-signed-integer-unit:1>>};
 %% int 64
-pack_int_(N) ->
+pack_int(N) ->
     {ok, <<16#D3:8, N:64/big-signed-integer-unit:1>>}.
 
 -spec pack_double(float()) -> binary().
@@ -270,7 +272,7 @@ pack_array(L) ->
 pack_array_([], Acc) ->
     {ok, list_to_binary(lists:reverse(Acc))};
 pack_array_([Term|Rest], Acc) ->
-    case pack_(Term) of
+    case pack_term(Term) of
         {ok, Binary} ->
             pack_array_(Rest, [Binary|Acc]);
         Error ->
@@ -297,9 +299,9 @@ pack_map(M)->
 pack_map_([], Acc) ->
     {ok, list_to_binary(lists:reverse(Acc))};
 pack_map_([{Key, Value}|Rest], Acc) ->
-    case pack_(Key) of
+    case pack_term(Key) of
         {ok, KeyBinary} ->
-            case pack_(Value) of
+            case pack_term(Value) of
                 {ok, ValueBinary} ->
                     pack_map_(Rest, [ValueBinary, KeyBinary|Acc]);
                 Error ->
@@ -311,12 +313,13 @@ pack_map_([{Key, Value}|Rest], Acc) ->
 pack_map_([Term|_Rest], _Acc) ->
     {error, {badarg, Term}}.
 
+
 -ifdef(TEST).
 
 test_([]) -> 0;
 test_([Term|Rest])->
-    {ok, Pack} = msgpack:pack(Term),
-    ?assertEqual({ok, Term}, msgpack:unpack(Pack)),
+    {ok, Binary} = msgpack:pack(Term),
+    ?assertEqual({ok, Term}, msgpack:unpack(Binary)),
     1 + test_(Rest).
 
 test_data()->
@@ -358,21 +361,20 @@ map_test_()->
     [
         {"map",
             fun() ->
-                    Ints = lists:seq(0, 65),
-                    Map = [ {X, X*2} || X <- Ints ] ++ [{<<"hage">>, 324}, {43542, [nil, true, false]}],
-                    {ok, Bin1} = msgpack:pack(Map),
-                    ?assertEqual({ok, Map}, msgpack:unpack(Bin1))
+                    Map = [ {X, X * 2} || X <- lists:seq(0, 65) ] ++ [{<<"hage">>, 324}, {43542, [nil, true, false]}],
+                    {ok, Binary} = msgpack:pack(Map),
+                    ?assertEqual({ok, Map}, msgpack:unpack(Binary))
             end},
         {"empty map is empty list ...",
             fun() ->
                     EmptyMap = [],
-                    {ok, Bin2} = msgpack:pack(EmptyMap),
-                    ?assertEqual({ok, EmptyMap}, msgpack:unpack(Bin2))
+                    {ok, Binary} = msgpack:pack(EmptyMap),
+                    ?assertEqual({ok, EmptyMap}, msgpack:unpack(Binary))
             end}
     ].
 
 incomplete_test()->
-    ?assertEqual({error, {incomplete, <<>>}}, msgpack:unpack(<<>>)).
+    ?assertEqual({error, incomplete}, msgpack:unpack(<<>>)).
 
 
 benchmark_test()->
@@ -384,10 +386,10 @@ benchmark_test()->
 
 error_test_()->
     [
-        {"",
+        {"badarg atom",
             ?_assertEqual({error, {badarg, atom}},
                           msgpack:pack(atom))},
-        {"",
+        {"badarg tuple",
             fun() ->
                     Term = {"hoge", "hage", atom},
                     ?assertEqual({error, {badarg, Term}},
@@ -395,12 +397,16 @@ error_test_()->
             end}
     ].
 
-long_binary_test()->
-    {ok, A} = msgpack:pack(1),
-    {ok, B} = msgpack:pack(10),
-    {ok, C} = msgpack:pack(100),
-    ?assertEqual({ok, [1,10,100]},
-                 unpack(<<A/binary, B/binary, C/binary>>)),
-    ok.
+long_binary_test_()->
+    [
+        {"long binary",
+            fun() ->
+                    {ok, A} = msgpack:pack(1),
+                    {ok, B} = msgpack:pack(10),
+                    {ok, C} = msgpack:pack(100),
+                    ?assertEqual({ok, [1,10,100]},
+                                 unpack(list_to_binary([A, B, C])))
+            end}
+    ].
 
 -endif.
