9633cb61778af1cd52316afb9a0cdd58e00f2595
Ryosuke Nakai <twisted.mind@voluntas.net>
2012-07-16 18:09:53 +0900

    Update qc type

--- test/msgpack_proper.erl
+++ test/msgpack_proper.erl
@@ -20,28 +20,28 @@ negative_fixnum() ->
     choose(-32, -1).
 
 int8() ->
-    choose(-16#80, 16#7F).
+    choose(-16#80, 0).
 
 int16() ->
-    choose(-16#8000, 16#7FFF).
+    choose(-16#8000, -16#80).
 
 int32() ->
-    choose(-16#80000000, 16#7FFFFFFF).
+    choose(-16#80000000, -16#8000).
 
 int64() ->
-    choose(-16#8000000000000000, 16#7FFFFFFFFFFFFFFF).
+    choose(-16#8000000000000000, -16#80000000).
 
 uint8() ->
     choose(0, 16#FF).
 
 uint16() ->
-    choose(0, 16#FFFF).
+    choose(16#FF, 16#FFFF).
 
 uint32() ->
-    choose(0, 16#FFFFFFFF).
+    choose(16#FFFF, 16#FFFFFFFF).
 
 uint64() ->
-    choose(0, 16#FFFFFFFFFFFFFFFF).
+    choose(16#FFFFFFFF, 16#FFFFFFFFFFFFFFFF).
 
 nil() ->
     nil.
@@ -51,24 +51,20 @@ fix_array() ->
          proper_gen:list_gen(Integer, choose_type())).
 
 %% array16() ->
-%%     ?LET(Integer, choose(16, 16#FFFF),
-%%          proper_gen:list_gen(Integer, choose_type())).
+%%      proper_gen:list_gen(16, choose_type()).
 
 %% array32() ->
-%%     ?LET(Integer, choose(16#FFFF, 16#010000),
-%%          proper_gen:list_gen(Integer, choose_type())).
+%%     [ N || N <- lists:seq(0, 16#010000)].
 
 fix_map() ->
     ?LET(Integer, choose(0, 15),
          {proper_gen:list_gen(Integer, {choose_type(), choose_type()})}).
 
 %% map16() ->
-%%     ?LET(Integer, choose(16, 16#FFFF),
-%%         proper_gen:list_gen(Integer, {choose_type(), choose_type()})).
+%%      proper_gen:list_gen(16, {choose_type(), choose_type()}).
 
 %% map32() ->
-%%     ?LET(Integer, choose(16#FFFF, 16#010000),
-%%         proper_gen:list_gen(Integer, {choose_type(), choose_type()})).
+%%     [{N, N * N} || N <- lists:seq(0, 16#010000)].
 
 fix_raw() ->
     ?LET(Integer, choose(0, 31),
