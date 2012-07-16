fd5e0d16275bb872671945d0f86517429c6aff96
Ryosuke Nakai <twisted.mind@voluntas.net>
2012-07-16 19:42:55 +0900

    Move msgpack test

--- test/msgpack_tests.erl
+++ test/msgpack_tests.erl
@@ -1,7 +1,117 @@
 -module(msgpack_tests).
 
+-import(msgpack, [pack/1, unpack/1]).
+
 -include_lib("eunit/include/eunit.hrl").
 
 msgpack_props_test_() ->
     {timeout,10000, ?_assertEqual([], proper:module(msgpack_props))}.
+
+unpack_test_() ->
+    [
+        {"not binary",
+            ?_assertEqual({error, {badarg, []}}, unpack([]))},
+
+        {"incomplete: null binary",
+            ?_assertEqual({error, incomplete}, unpack(<<>>))},
+
+        {"incomplete: unknown binary",
+            ?_assertEqual({error, incomplete}, unpack(<<16#DA>>))}
+    ].
+
+array_test_()->
+    [
+        {"length 16",
+            fun() ->
+                    List = lists:seq(0, 16),
+                    {ok, Binary} = pack(List),
+                    ?assertEqual({ok, List}, unpack(Binary))
+            end},
+        {"length 32",
+            fun() ->
+                    List = lists:seq(0, 16#010000),
+                    {ok, Binary} = pack(List),
+                    ?assertEqual({ok, List}, unpack(Binary))
+            end},
+        {"empty",
+            fun() ->
+                    EmptyList = [],
+                    {ok, Binary} = pack(EmptyList),
+                    ?assertEqual({ok, EmptyList}, unpack(Binary))
+            end}
+    ].
+
+
+map_test_()->
+    [
+        {"length 16",
+            fun() ->
+                    Map = {[ {X, X * 2} || X <- lists:seq(0, 16) ]},
+                    {ok, Binary} = pack(Map),
+                    ?assertEqual({ok, Map}, unpack(Binary))
+            end},
+        {"length 32",
+            fun() ->
+                    Map = {[ {X, X * 2} || X <- lists:seq(0, 16#010000) ]},
+                    {ok, Binary} = pack(Map),
+                    ?assertEqual({ok, Map}, unpack(Binary))
+            end},
+        {"empty",
+            fun() ->
+                    EmptyMap = {[]},
+                    {ok, Binary} = pack(EmptyMap),
+                    ?assertEqual({ok, EmptyMap}, unpack(Binary))
+            end}
+    ].
+
+int_test_() ->
+    [
+        {"",
+            fun() ->
+                    Term = -2147483649,
+                    {ok, Binary} = pack(Term),
+                    ?assertEqual({ok, Term}, unpack(Binary))
+            end}
+    ].
+
+error_test_()->
+    [
+        {"badarg atom",
+            ?_assertEqual({error, {badarg, atom}},
+                          pack(atom))},
+        {"badarg tuple",
+            fun() ->
+                    Term = {"hoge", "hage", atom},
+                    ?assertEqual({error, {badarg, Term}},
+                                 pack(Term))
+            end}
+    ].
+
+binary_test_() ->
+    [
+        {"0 byte",
+            fun() ->
+                    {ok, Binary} = pack(<<>>),
+                    ?assertEqual({ok, <<>>}, unpack(Binary))
+            end}
+    ].
+
+long_binary_test_()->
+    [
+        {"long binary",
+            fun() ->
+                    {ok, A} = pack(1),
+                    {ok, B} = pack(10),
+                    {ok, C} = pack(100),
+                    ?assertEqual({ok, [1,10,100]},
+                                 unpack(list_to_binary([A, B, C])))
+            end}
+    ].
+
+%% benchmark_test()->
+%%     Data = [test_data() || _ <- lists:seq(0, 10000)],
+%%     {ok, S} = ?debugTime("  serialize", pack(Data)),
+%%     {ok, Data} = ?debugTime("deserialize", unpack(S)),
+%%     ?debugFmt("for ~p KB test data.", [byte_size(S) div 1024]),
+%%     ok.
 
